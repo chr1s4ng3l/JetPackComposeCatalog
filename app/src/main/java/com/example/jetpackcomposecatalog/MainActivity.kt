8ee76def8a41f5a6be50fb00cd9ab29e3693d3b4
Consultant <christopher.tamayocazarez@gmail.com>
2023-03-02 21:21:56 -0500

    Icons

--- app/src/main/java/com/example/jetpackcomposecatalog/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecatalog/MainActivity.kt
@@ -5,18 +5,22 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
@@ -32,8 +36,7 @@ class MainActivity : ComponentActivity() {
             JetPackComposeCatalogTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
 //                    var myText by remember {
 //                        mutableStateOf("Christopher")
@@ -42,8 +45,7 @@ class MainActivity : ComponentActivity() {
 //                        myText = it
 //                    }
 
-                    MyButtonExample()
-
+                    MyImage()
                 }
             }
         }
@@ -55,7 +57,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     JetPackComposeCatalogTheme {
-        MyButtonExample()
+        MyIcon()
     }
 }
 
@@ -66,31 +68,23 @@ fun MyTextFieldAdvance() {
     }
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        OutlinedTextField(
-            value = myText,
-            onValueChange = {
-                myText = if (it.contains("a")) {
-                    it.replace("a", "*")
-                } else {
-                    it
-                }
+        OutlinedTextField(value = myText, onValueChange = {
+            myText = if (it.contains("a")) {
+                it.replace("a", "*")
+            } else {
+                it
+            }
 
 
-            },
-            label = { Text(text = "Type your name") },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Email,
-                    contentDescription = ""
-                )
-            },
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = Color.Blue,
-                unfocusedBorderColor = Color.Green
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
+        }, label = { Text(text = "Type your name") }, leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Email, contentDescription = ""
+            )
+        }, colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Blue, unfocusedBorderColor = Color.Green
+        ), modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
         )
 
     }
@@ -119,13 +113,9 @@ fun MyButtonExample() {
     ) {
 
         Button(
-            onClick = { enable = false },
-            enabled = enable,
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color.Red,
-                contentColor = Color.White
-            ),
-            border = BorderStroke(10.dp, Color.Green)
+            onClick = { enable = false }, enabled = enable, colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Red, contentColor = Color.White
+            ), border = BorderStroke(10.dp, Color.Green)
 
         ) {
 
@@ -134,7 +124,7 @@ fun MyButtonExample() {
         }
 
         OutlinedButton(
-            onClick = {enable = false},
+            onClick = { enable = false },
             enabled = enable,
             modifier = Modifier.padding(top = 8.dp),
             colors = ButtonDefaults.buttonColors(
@@ -147,9 +137,31 @@ fun MyButtonExample() {
             Text(text = "Hi there")
         }
 
+        TextButton(onClick = {}) {
+            Text(text = "LOLOLO")
+        }
+
     }
 }
 
+
+@Composable
+fun MyIcon() {
+    Icon(imageVector = Icons.Rounded.Star, contentDescription = "StarIcon", tint = Color.Yellow)
+}
+
+@Composable
+fun MyImage() {
+
+    Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "Example",
+        modifier = Modifier
+            .padding(10.dp)
+            .border(5.dp, color = Color.Green)
+            .size(40.dp)
+    )
+}
 
 @Composable
 fun MyText() {
@@ -163,11 +175,9 @@ fun MyText() {
         Text(text = "My Text", textDecoration = TextDecoration.LineThrough)
         Text(text = "My Text", textDecoration = TextDecoration.Underline)
         Text(
-            text = "My Text",
-            textDecoration = TextDecoration.combine(
+            text = "My Text", textDecoration = TextDecoration.combine(
                 listOf(
-                    TextDecoration.LineThrough,
-                    TextDecoration.Underline
+                    TextDecoration.LineThrough, TextDecoration.Underline
                 )
             )
         )
